97c49f075a0166281cf946fa99bb08b035f54300
Zhongren Shao <shao.zhongren@gmail.com>
2021-11-22 21:58:17 -0600

    added some of processor.rs

--- src/instruction.rs
+++ src/instruction.rs
@@ -27,12 +27,12 @@ impl EscrowInstruction {
     pub fn unpack(input: &[u8]) -> Result<Self, ProgramError> {
         let (tag, rest) = input.split_first().ok_or(InvalidInstruction)?;
 
-        Ok(match tag {
-            0 => Self::InitEscrow {
+        match tag {
+            0 => Ok(Self::InitEscrow {
                 amount: Self::unpack_amount(rest)?,
-            },
-            _ => return Err(InvalidInstruction.into()),
-        })
+            }),
+            _ => Err(InvalidInstruction.into()),
+        }
     }
 
     fn unpack_amount(input: &[u8]) -> Result<u64, ProgramError> {
