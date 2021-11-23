e14af83777206e1f47f7d48c1c44d3916c9c6494
Zhongren Shao <shao.zhongren@gmail.com>
2021-11-23 00:29:26 -0600

    added unpack

--- src/state.rs
+++ src/state.rs
@@ -1,5 +1,7 @@
+use arrayref::{array_mut_ref, array_ref, array_refs, mut_array_refs};
 use solana_program::{
-    program_pack::{IsInitialized, Sealed},
+    program_error::ProgramError,
+    program_pack::{IsInitialized, Pack, Sealed},
     pubkey::Pubkey,
 };
 
@@ -12,8 +14,43 @@ pub struct Escrow {
 }
 
 impl Sealed for Escrow {}
+
 impl IsInitialized for Escrow {
     fn is_initialized(&self) -> bool {
         self.is_initialized
+    }
+}
+
+impl Pack for Escrow {
+    const LEN: usize = 105;
+
+    fn pack_into_slice(&self, dst: &mut [u8]) {
+        todo!()
+    }
+
+    fn unpack_from_slice(src: &[u8]) -> Result<Self, ProgramError> {
+        let src = array_ref![src, 0, Escrow::LEN];
+        let (
+            is_initialized,
+            initializer_pubkey,
+            temp_token_account_pubkey,
+            initializer_token_to_receive_account_pubkey,
+            expected_amount,
+        ) = array_refs![src, 1, 32, 32, 32, 8];
+        let is_initialized = match is_initialized {
+            [0] => false,
+            [1] => true,
+            _ => return Err(ProgramError::InvalidAccountData),
+        };
+
+        Ok(Escrow {
+            is_initialized,
+            initializer_pubkey: Pubkey::new_from_array(*initializer_pubkey),
+            temp_token_account_pubkey: Pubkey::new_from_array(*temp_token_account_pubkey),
+            initializer_token_to_receive_account_pubkey: Pubkey::new_from_array(
+                *temp_token_account_pubkey,
+            ),
+            expected_amount: u64::from_le_bytes(*expected_amount),
+        })
     }
 }
